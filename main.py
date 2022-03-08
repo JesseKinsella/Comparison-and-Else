d6d7d7480ab00b4f3b5837155b9bfba8ff3ab945
JesseKinsella1 <89218189+JesseKinsella@users.noreply.github.com>
2022-03-08 14:44:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+light_is_on = False
+
+if light_is_on:
+  print("The Light is On!")
+else:
+  print("We're in the Dark!")
+#else gives you false side
+
+weight = 160.5
+
+if weight < 170:
+  print("You're under weight :D")
+#made a comparison on float, it generates a boolean
+
+# == means exactly equal to and != means not equal to
+
+
+age = 18
+
+if age <= 17:
+  print("Child")
+else:
+  print("Adult")
